c2ceb744a657be1f3ec8ee011bc8f9b00de58a1d
sijomonps <sijomon700@gmail.com>
2025-11-21 06:37:03 +0530

    image deployment

--- my_site/settings.py
+++ my_site/settings.py
@@ -155,3 +155,7 @@ AUTH_USER_MODEL = 'pages.CustomUser'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/'
+
+if not DEBUG:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
